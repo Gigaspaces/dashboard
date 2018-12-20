db8d6991134737ebff986c7a75ee1daafae21329
Yohana Khoury <yohana@yohanakhoury.com>
2018-12-20 17:57:32 +0200

    compile with stack + fix compilation error with ClientEnv

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,5 @@
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RankNTypes #-}
@@ -9,87 +7,48 @@
 {-# LANGUAGE TypeOperators #-}
 
 module Main where
-
 import Data.Text
-import Servant.API
+import Network.HTTP.Client (newManager, defaultManagerSettings, Manager)
+import Network.HTTP.Client.TLS (tlsManagerSettings, newTlsManager, mkManagerSettings, newTlsManagerWith)
+import Servant.Client
+import Network.Connection         (TLSSettings(..))
+import Servant.API.BasicAuth      (BasicAuthData (BasicAuthData))
+import Client.Timeline
+import Client.Newman.Jobs
+import Client.Util (getBasicAuthData)
 
-import Lib
-
-
-import Data.Aeson.Compat
-import Data.Aeson.Types
-import Data.Attoparsec.ByteString
-import GHC.Generics
-import qualified Data.Aeson.Parser
-
-import Data.Time.Calendar
-import Servant
-import Network.Wai
-import Network.Wai.Handler.Warp
-
-import Network.HTTP.Types
-import Network.WebSockets.Connection (defaultConnectionOptions, sendTextData, acceptRequest)
-import Network.Wai.Handler.WebSockets (websocketsOr)
-import Network.WebSockets (ServerApp)
-
-type MyAPI = "users" :> Get '[JSON] [User]
-    :<|> "static" :> Raw
-    :<|> "websocket" :> Raw
-
-data User = User
-  { name :: String
-  , age :: Int
-  , email :: String
-  , registration_date :: Day
-  } deriving (Eq, Show, Generic)
-
-instance ToJSON User
-
-users1 :: [User]
-users1 =
-  [ User "Isaac Newton"    372 "isaac@newton.co.uk" (fromGregorian 1683  3 1)
-  , User "Albert Einstein" 136 "ae@mc2.org"         (fromGregorian 1905 12 1)
-  ]
-
-
-myAPI :: Proxy MyAPI
-myAPI = Proxy
-
-
-server :: Server MyAPI
-server = return users1
-    :<|> serveDirectoryWebApp "static-files"
-    :<|> websocketfunc
+-- :set -XOverloadedStrings
 
 
 
-app1 :: Application
-app1 = serve myAPI server
 
-
-websocketfunc :: Tagged Handler Application
-websocketfunc = Tagged $ app2
-
---websocketfunc = Tagged $ return (\respond  ->  respond helloworld')
---      where helloworld' = responseLBS
---                               status200
---                               [("Content-Type", "text/plain")]
---                               "Hello, Web!"
-
-
-app2 :: Application
-app2 = websocketsOr defaultConnectionOptions wsApp backupApp
-  where
-    wsApp :: ServerApp
-    wsApp pending_conn = do
-        conn <- acceptRequest pending_conn
-        sendTextData conn ("Hello, client!" :: Text)
-
-    backupApp :: Application
-    backupApp _ respond = respond $ responseLBS status400 [] "Not a WebSocket request"
-
---type Application = Request -> (Response -> IO ResponseReceived) -> IO ResponseReceived
+getSecureManager :: IO Manager
+getSecureManager = let tlsSettings = TLSSettingsSimple True False False -- Disable certificate verification completely
+                   in newTlsManagerWith (mkManagerSettings tlsSettings Nothing)
 
 
 main :: IO ()
-main = run 8081 app1
+main = do
+  putStrLn ""
+  authData <- getBasicAuthData "BasicAuthData.json"
+  case authData of
+    Nothing -> putStrLn "BasicAuthData.json not found"
+    Just user -> do manager' <- newManager defaultManagerSettings
+                    -- http://192.168.10.2:6060/api/timeline
+                    res <- runClientM getTimeline (ClientEnv manager' (BaseUrl Http "192.168.10.2" 6060 "/api") Nothing)
+                    case res of
+                      Left err -> putStrLn $ "Error: " ++ show err
+                      Right timeline -> print timeline
+                    -- https://xap-newman.gspaces.com:8443/api/newman/
+                    --  secureManager <- newManager tlsManagerSettings
+                    putStrLn ""
+                    secureManager <- getSecureManager
+                    res <- runClientM (getJobs user (Just 1) (Just "-submitTime")) (ClientEnv secureManager (BaseUrl Https "xap-newman.gspaces.com" 8443 "/api/newman")  Nothing)
+                    case res of
+                      Left err -> putStrLn $ "Error: " ++ show err
+                      Right jobs -> print jobs
+                    putStrLn ""
+                    res <- runClientM (getJobsConfig user) (ClientEnv secureManager (BaseUrl Https "xap-newman.gspaces.com" 8443 "/api/newman")  Nothing)
+                    case res of
+                      Left err -> putStrLn $ "Error: " ++ show err
+                      Right jobsConfig -> print jobsConfig
---
+++ app/Main2.hs
@@ -0,0 +1,95 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Main2 where
+
+import Data.Text
+import Servant.API
+
+import Lib
+
+
+import Data.Aeson.Compat
+import Data.Aeson.Types
+import Data.Attoparsec.ByteString
+import GHC.Generics
+import qualified Data.Aeson.Parser
+
+import Data.Time.Calendar
+import Servant
+import Network.Wai
+import Network.Wai.Handler.Warp
+
+import Network.HTTP.Types
+import Network.WebSockets.Connection (defaultConnectionOptions, sendTextData, acceptRequest)
+import Network.Wai.Handler.WebSockets (websocketsOr)
+import Network.WebSockets (ServerApp)
+
+type MyAPI = "users" :> Get '[JSON] [User]
+    :<|> "static" :> Raw
+    :<|> "websocket" :> Raw
+
+data User = User
+  { name :: String
+  , age :: Int
+  , email :: String
+  , registration_date :: Day
+  } deriving (Eq, Show, Generic)
+
+instance ToJSON User
+
+users1 :: [User]
+users1 =
+  [ User "Isaac Newton"    372 "isaac@newton.co.uk" (fromGregorian 1683  3 1)
+  , User "Albert Einstein" 136 "ae@mc2.org"         (fromGregorian 1905 12 1)
+  ]
+
+
+myAPI :: Proxy MyAPI
+myAPI = Proxy
+
+
+server :: Server MyAPI
+server = return users1
+    :<|> serveDirectoryWebApp "static-files"
+    :<|> websocketfunc
+
+
+
+app1 :: Application
+app1 = serve myAPI server
+
+
+websocketfunc :: Tagged Handler Application
+websocketfunc = Tagged $ app2
+
+--websocketfunc = Tagged $ return (\respond  ->  respond helloworld')
+--      where helloworld' = responseLBS
+--                               status200
+--                               [("Content-Type", "text/plain")]
+--                               "Hello, Web!"
+
+
+app2 :: Application
+app2 = websocketsOr defaultConnectionOptions wsApp backupApp
+  where
+    wsApp :: ServerApp
+    wsApp pending_conn = do
+        conn <- acceptRequest pending_conn
+        sendTextData conn ("Hello, client!" :: Text)
+
+    backupApp :: Application
+    backupApp _ respond = respond $ responseLBS status400 [] "Not a WebSocket request"
+
+--type Application = Request -> (Response -> IO ResponseReceived) -> IO ResponseReceived
+
+
+main :: IO ()
+main = run 8081 app1
